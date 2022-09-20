e5cec6e2d9fc8ba7ed5cd6a76261824522a4a782
brianreicher <reicher.b@northeastern.edu>
2022-09-20 11:41:49 -0400

    Adding final problems

---
+++ sql-ex/ex-sq-12.sql
@@ -0,0 +1,21 @@
+-- 1
+SELECT ShipCity
+FROM Employees NATURAL JOIN Orders
+WHERE FirstName = 'Michael' AND LastName = 'Suyama' AND
+      ShipCountry in ('Ireland', 'Poland');
+
+
+-- 2a
+SELECT ProductName
+FROM Suppliers NATURAL JOIN Products
+WHERE Country='Germany';
+
+-- 2b
+SELECT ProductName
+FROM Suppliers NATURAL JOIN Products
+WHERE Country='Germany' AND UnitsOnOrder>0;
+
+-- 3
+SELECT CompanyName
+FROM Suppliers NATURAL JOIN Categories
+WHERE CategoryName='Confections'
