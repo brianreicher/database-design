93d817ee6ae5d067d71e207ceb46421174dce78f
brianreicher <reicher.b@northeastern.edu>
2022-11-04 11:11:06 -0400

    MySQL initial commit

---
+++ mysql_docker/mysql_init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Enrollment;
+
+SHOW DATABASES;
+
+USE Enrollment;
+
+
+GRANT ALL PRIVILEGES ON Enrollment.* TO 'root'@'%';
+
+CREATE TABLE students (
+    nu_id char(10) PRIMARY KEY,
+    firstName varchar(50)
+);
+
+SHOW TABLES;
